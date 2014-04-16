f2ffe6a5f92c32edc11b02a29a87b8d32330373e
Dzikri Aziz <kvcrvt@gmail.com>
2014-04-17 04:02:41 +0700

    Bump version: 0.2.3

--- menu-icons.php
+++ menu-icons.php
@@ -4,14 +4,14 @@
  * Menu Icons
  *
  * @package Menu_Icons
- * @version 0.2.2
+ * @version 0.2.3
  * @author Dzikri Aziz <kvcrvt@gmail.com>
  *
  *
  * Plugin name: Menu Icons
  * Plugin URI: http://kucrut.org/
  * Description: Easily add icons to your navigation menu items
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Dzikri Aziz
  * Author URI: http://kucrut.org/
  * License: GPLv2
@@ -24,7 +24,7 @@
  */
 final class Menu_Icons {
 
-	const VERSION = '0.2.2';
+	const VERSION = '0.2.3';
 
 	/**
 	 * Holds plugin data
