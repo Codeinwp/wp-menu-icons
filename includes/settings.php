0d06d68ed165c57e67743cd7dc14b4e2f9696333
Dzikri Aziz <kvcrvt@gmail.com>
2014-04-24 17:47:38 +0700

    Implement settings reset

--- includes/settings.php
+++ includes/settings.php
@@ -8,6 +8,12 @@
  */
 final class Menu_Icons_Settings {
 
+	const UPDATE_KEY = 'menu-icons-settings-update';
+
+	const RESET_KEY = 'menu-icons-settings-reset';
+
+	const TRANSIENT_KEY = 'menu_icons_message';
+
 	protected static $defaults = array(
 		'icon_types' => array(),
 		'extra_css'  => '1',
@@ -64,7 +70,7 @@ final class Menu_Icons_Settings {
 		self::$defaults['icon_types'] = array_keys( Menu_Icons::get('icon_types') );
 		self::_get();
 
-		add_action( 'load-nav-menus.php', array( __CLASS__, '_load_nav_menus' ) );
+		add_action( 'load-nav-menus.php', array( __CLASS__, '_load_nav_menus' ), 1 );
 	}
 
 
@@ -72,6 +78,7 @@ final class Menu_Icons_Settings {
 		self::_maybe_update_settings();
 		self::_add_settings_meta_box();
 
+		add_action( 'admin_notices', array( __CLASS__, '_admin_notices' ) );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, '_enqueue_scripts' ), 99 );
 
 		// Load editor
@@ -92,11 +99,44 @@ final class Menu_Icons_Settings {
 	 */
 	public static function _maybe_update_settings() {
 		if ( ! empty( $_POST['menu-icons']['settings'] ) ) {
-			$values = update_option(
-				'menu-icons',
-				$_POST['menu-icons']['settings']
+			check_admin_referer( self::UPDATE_KEY, self::UPDATE_KEY );
+
+			update_option( 'menu-icons', $_POST['menu-icons']['settings'] );
+			set_transient( self::TRANSIENT_KEY, 'updated', 30 );
+			wp_redirect(
+				remove_query_arg(
+					array( 'menu-icons-reset' ),
+					wp_get_referer()
+				)
 			);
-			self::_get();
+		}
+		elseif ( ! empty( $_REQUEST[ self::RESET_KEY ] ) ) {
+			check_admin_referer( self::RESET_KEY, self::RESET_KEY );
+
+			delete_option( 'menu-icons' );
+			set_transient( self::TRANSIENT_KEY, 'reset', 30 );
+			wp_redirect(
+				remove_query_arg(
+					array( self::RESET_KEY, 'menu-icons-updated' ),
+					wp_get_referer()
+				)
+			);
+		}
+	}
+
+
+	public static function _admin_notices() {
+		$messages = array(
+			'updated' => __( '<strong>Menu Icons Settings</strong> has been successfully updated.', 'menu-icons' ),
+			'reset'   => __( '<strong>Menu Icons Settings</strong> has been successfully reset.', 'menu-icons' ),
+		);
+
+		$message_type = get_transient( self::TRANSIENT_KEY );
+		if ( ! empty( $message_type ) && ! empty( $messages[ $message_type ] ) ) {
+			printf(
+				'<div class="updated"><p>%s</p></div>',
+				wp_kses( $messages[ $message_type ], array( 'strong' => true ) )
+			);
 		}
 	}
 
@@ -184,10 +224,17 @@ final class Menu_Icons_Settings {
 				</div>
 			</div>
 			<p class="submitbox button-controls">
+				<?php wp_nonce_field( self::UPDATE_KEY, self::UPDATE_KEY ) ?>
 				<span class="list-controls">
 					<?php printf(
 						'<a href="%s" class="select-all submitdelete">%s</a>',
-						esc_url( '#' ),
+						esc_url(
+							wp_nonce_url(
+								admin_url( '/nav-menus.php' ),
+								self::RESET_KEY,
+								self::RESET_KEY
+							)
+						),
 						esc_html__( 'Reset', 'menu-icons' )
 					) ?>
 				</span>
