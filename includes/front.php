7414a99b7efac3aeb8249abd58d62b4eaf91cda6
Dzikri Aziz <kvcrvt@gmail.com>
2015-11-05 14:14:59 +0700

    Enqueue icon type stylesheets on the front-end

--- includes/front.php
+++ includes/front.php
@@ -9,13 +9,34 @@
 final class Menu_Icons_Front_End {
 
 	/**
+	 * Icon types
+	 *
+	 * @since  0.9.0
+	 * @access protected
+	 * @var    array
+	 */
+	protected static $icon_types = array();
+
+
+	/**
 	 * Add hooks for front-end
 	 *
 	 * @since 0.9.0
 	 */
 	public static function init() {
-		// TODO: Get registered icon types from Icon_Picker
-		// TODO: Call front-end functions of each icon types
+		$active_types = Menu_Icons_Settings::get( 'global', 'icon_types' );
+
+		if ( empty( $active_types ) ) {
+			return;
+		}
+
+		foreach ( Icon_Picker_Types_Registry::instance()->types as $type ) {
+			if ( in_array( $type->id, $active_types ) ) {
+				self::$icon_types[] = $type;
+			}
+		}
+
+		// TODO: Call front-end calback of each icon types
 
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, '_enqueue_styles' ), 7 );
 	}
@@ -30,7 +51,11 @@ final class Menu_Icons_Front_End {
 	 * @link    http://codex.wordpress.org/Plugin_API/Action_Reference/wp_enqueue_scripts
 	 */
 	public static function _enqueue_styles() {
-		// TODO: Enqueue icon types' stylesheets
+		foreach ( self::$icon_types as $type ) {
+			if ( wp_style_is( $type->stylesheet_id, 'registered' ) ) {
+				wp_enqueue_style( $type->stylesheet_id );
+			}
+		}
 
 		wp_enqueue_style(
 			'menu-icons-extra',
