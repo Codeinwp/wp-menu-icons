bdb5bc20711cec4890dde037e2037a5779e0b990
Dzikri Aziz <kvcrvt@gmail.com>
2015-11-09 12:19:16 +0700

    Introduce `kucrut_get_image_sizes()`

--- includes/library/functions.php
+++ includes/library/functions.php
@@ -64,3 +64,33 @@ if ( ! function_exists( 'kucrut_validate' ) ) {
 		return $values;
 	}
 }
+
+if ( ! function_exists( 'kucrut_get_image_sizes' ) ) {
+	/**
+	 * Get image sizes
+	 *
+	 * @since  0.9.0
+	 * @access protected
+	 * @return array
+	 */
+	function kucrut_get_image_sizes() {
+		$_sizes = array(
+			'thumbnail' => __( 'Thumbnail', 'menu-icons' ),
+			'medium'    => __( 'Medium', 'menu-icons' ),
+			'large'     => __( 'Large', 'menu-icons' ),
+			'full'      => __( 'Full Size', 'menu-icons' ),
+		);
+
+		$_sizes = apply_filters( 'image_size_names_choose', $_sizes );
+
+		$sizes = array();
+		foreach ( $_sizes as $value => $label ) {
+			$sizes[] = array(
+				'value' => $value,
+				'label' => $label,
+			);
+		}
+
+		return $sizes;
+	}
+}
