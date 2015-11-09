ce10cb429d32b00737bbb43956c5918fb4472446
Dzikri Aziz <kvcrvt@gmail.com>
2015-11-09 12:40:10 +0700

    Add title and info elements on the sidebar

--- js/admin.js
+++ js/admin.js
@@ -115,9 +115,28 @@ module.exports = MenuIcons;
  * wp.media.view.MenuIconsSidebar
  */
 var MenuIconsSidebar = wp.media.view.IconPickerSidebar.extend({
-	createSingle: function() {
-		console.log( 'create single' );
+	initialize: function() {
+		var title = new wp.media.View({
+			tagName:  'h3',
+			priority: -10
+		});
+
+		var info = new wp.media.View({
+			tagName:   'p',
+			className: '_info',
+			priority:  1000
+		});
+
+		wp.media.view.IconPickerSidebar.prototype.initialize.apply( this, arguments );
+
+		title.$el.text( menuIcons.text.preview );
+		this.set( 'title', title );
+
+		info.$el.html( menuIcons.text.settingsInfo );
+		this.set( 'info', info );
 	},
+
+	createSingle: function() {},
 	disposeSingle: function() {}
 });
 
