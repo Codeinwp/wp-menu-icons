55f327e90fb7b6ea1569b0db2048ac86fcffa490
Dzikri Aziz <kvcrvt@gmail.com>
2017-02-11 14:12:23 +0800

    Lint

--- js/src/picker.js
+++ js/src/picker.js
@@ -1,188 +1,187 @@
-(function( $ ) {
-'use strict';
+/* global menuIcons: true */
+( function( $ ) {
+	var miPicker;
 
-var miPicker;
-
-if ( ! menuIcons.activeTypes || _.isEmpty( menuIcons.activeTypes ) ) {
-	return;
-}
-
-/**
- * @namespace
- * @property {object} templates - Cached templates for the item previews on the fields
- * @property {string} wrapClass - Field wrapper's class
- * @property {object} frame     - Menu Icons' media frame instance
- * @property {object} target    - Frame's target model
- */
-miPicker = {
-	templates: {},
-	wrapClass: 'div.menu-icons-wrap',
-	frame: null,
-	target: new wp.media.model.IconPickerTarget(),
+	if ( ! menuIcons.activeTypes || _.isEmpty( menuIcons.activeTypes ) ) {
+		return;
+	}
 
 	/**
-	 * Callback function to filter active icon types
-	 *
-	 * TODO: Maybe move to frame view?
-	 *
-	 * @param {string} type - Icon type.
+	 * @namespace
+	 * @property {object} templates - Cached templates for the item previews on the fields
+	 * @property {string} wrapClass - Field wrapper's class
+	 * @property {object} frame     - Menu Icons' media frame instance
+	 * @property {object} target    - Frame's target model
 	 */
-	typesFilter: function( type ) {
-		return ( -1 < $.inArray( type.id, menuIcons.activeTypes ) );
-	},
+	miPicker = {
+		templates: {},
+		wrapClass: 'div.menu-icons-wrap',
+		frame: null,
+		target: new wp.media.model.IconPickerTarget(),
 
-	/**
-	 * Create Menu Icons' media frame
-	 */
-	createFrame: function() {
-		miPicker.frame = new wp.media.view.MediaFrame.MenuIcons({
-			target:      miPicker.target,
-			ipTypes:     _.filter( iconPicker.types, miPicker.typesFilter ),
-			SidebarView: wp.media.view.MenuIconsSidebar
-		});
-	},
+		/**
+		 * Callback function to filter active icon types
+		 *
+		 * TODO: Maybe move to frame view?
+		 *
+		 * @param {string} type - Icon type.
+		 */
+		typesFilter: function( type ) {
+			return ( $.inArray( type.id, menuIcons.activeTypes ) >= 0 );
+		},
 
-	/**
-	 * Pick icon for a menu item and open the frame
-	 *
-	 * @param {object} model - Menu item model.
-	 */
-	pickIcon: function( model ) {
-		miPicker.frame.target.set( model, { silent: true } );
-		miPicker.frame.open();
-	},
+		/**
+		 * Create Menu Icons' media frame
+		 */
+		createFrame: function() {
+			miPicker.frame = new wp.media.view.MediaFrame.MenuIcons({
+				target:      miPicker.target,
+				ipTypes:     _.filter( iconPicker.types, miPicker.typesFilter ),
+				SidebarView: wp.media.view.MenuIconsSidebar
+			});
+		},
 
-	/**
-	 * Set or unset icon
-	 *
-	 * @param {object} e - jQuery click event.
-	 */
-	setUnset: function( e ) {
-		var $el      = $( e.currentTarget ),
-		    $clicked = $( e.target );
+		/**
+		 * Pick icon for a menu item and open the frame
+		 *
+		 * @param {object} model - Menu item model.
+		 */
+		pickIcon: function( model ) {
+			miPicker.frame.target.set( model, { silent: true });
+			miPicker.frame.open();
+		},
 
-		e.preventDefault();
+		/**
+		 * Set or unset icon
+		 *
+		 * @param {object} e - jQuery click event.
+		 */
+		setUnset: function( e ) {
+			var $el      = $( e.currentTarget ),
+			    $clicked = $( e.target );
 
-		if ( $clicked.hasClass( '_select' ) || $clicked.hasClass( '_icon' ) ) {
-			miPicker.setIcon( $el );
-		} else if ( $clicked.hasClass( '_remove' ) ) {
-			miPicker.unsetIcon( $el );
-		}
-	},
+			e.preventDefault();
 
-	/**
-	 * Set Icon
-	 *
-	 * @param {object} $el - jQuery object.
-	 */
-	setIcon: function( $el ) {
-		var id     = $el.data( 'id' ),
-		    frame  = miPicker.frame,
-		    items  = frame.menuItems,
-		    model  = items.get( id );
+			if ( $clicked.hasClass( '_select' ) || $clicked.hasClass( '_icon' ) ) {
+				miPicker.setIcon( $el );
+			} else if ( $clicked.hasClass( '_remove' ) ) {
+				miPicker.unsetIcon( $el );
+			}
+		},
 
-		if ( model ) {
-			miPicker.pickIcon( model.toJSON() );
-			return;
-		}
+		/**
+		 * Set Icon
+		 *
+		 * @param {object} $el - jQuery object.
+		 */
+		setIcon: function( $el ) {
+			var id     = $el.data( 'id' ),
+			    frame  = miPicker.frame,
+			    items  = frame.menuItems,
+			    model  = items.get( id );
 
-		model = {
-			id:      id,
-			$el:     $el,
-			$title:  $( '#edit-menu-item-title-' + id ),
-			$inputs: {}
-		};
-
-		// Collect menu item's settings fields and use them
-		// as the model's attributes.
-		$el.find( 'div._settings input' ).each( function() {
-			var $input = $( this ),
-			    key    = $input.attr( 'class' ).replace( '_mi-', '' ),
-			    value  = $input.val();
-
-			if ( ! value ) {
-				if ( _.has( menuIcons.menuSettings, key ) ) {
-					value = menuIcons.menuSettings[ key ];
-				} else if ( _.has( menuIcons.settingsFields, key ) ) {
-					value = menuIcons.settingsFields[ key ]['default'];
-				}
+			if ( model ) {
+				miPicker.pickIcon( model.toJSON() );
+				return;
 			}
 
-			model[ key ]         = value;
-			model.$inputs[ key ] = $input;
-		});
+			model = {
+				id:      id,
+				$el:     $el,
+				$title:  $( '#edit-menu-item-title-' + id ),
+				$inputs: {}
+			};
 
-		items.add( model );
-		miPicker.pickIcon( model );
-	},
+			// Collect menu item's settings fields and use them
+			// as the model's attributes.
+			$el.find( 'div._settings input' ).each( function() {
+				var $input = $( this ),
+				    key    = $input.attr( 'class' ).replace( '_mi-', '' ),
+				    value  = $input.val();
 
-	/**
-	 * Unset icon
-	 *
-	 * @param {object} $el - jQuery object.
-	 */
-	unsetIcon: function( $el ) {
-		var id = $el.data( 'id' );
+				if ( ! value ) {
+					if ( _.has( menuIcons.menuSettings, key ) ) {
+						value = menuIcons.menuSettings[ key ];
+					} else if ( _.has( menuIcons.settingsFields, key ) ) {
+						value = menuIcons.settingsFields[ key ]['default'];
+					}
+				}
 
-		$el.find( 'div._settings input' ).val( '' );
-		$el.trigger( 'mi:update' );
-		miPicker.frame.menuItems.remove( id );
-	},
+				model[ key ]         = value;
+				model.$inputs[ key ] = $input;
+			});
 
-	/**
-	 * Update valeus of menu item's setting fields
-	 *
-	 * When the type and icon is set, this will (re)generate the icon
-	 * preview on the menu item field.
-	 *
-	 * @param {object} e - jQuery event.
-	 */
-	updateField: function( e ) {
-		var $el    = $( e.currentTarget ),
-		    $set   = $el.find( 'a._select' ),
-		    $unset = $el.find( 'a._remove' ),
-		    type   = $el.find( 'input._mi-type' ).val(),
-		    icon   = $el.find( 'input._mi-icon' ).val(),
-		    url    = $el.find( 'input._mi-url' ).val(),
-		    template;
+			items.add( model );
+			miPicker.pickIcon( model );
+		},
 
-		if ( '' === type || '' === icon || 0 > _.indexOf( menuIcons.activeTypes, type ) ) {
-			$set.text( menuIcons.text.select ).attr( 'title', '' );
-			$unset.addClass( 'hidden' );
+		/**
+		 * Unset icon
+		 *
+		 * @param {object} $el - jQuery object.
+		 */
+		unsetIcon: function( $el ) {
+			var id = $el.data( 'id' );
 
-			return;
+			$el.find( 'div._settings input' ).val( '' );
+			$el.trigger( 'mi:update' );
+			miPicker.frame.menuItems.remove( id );
+		},
+
+		/**
+		 * Update valeus of menu item's setting fields
+		 *
+		 * When the type and icon is set, this will (re)generate the icon
+		 * preview on the menu item field.
+		 *
+		 * @param {object} e - jQuery event.
+		 */
+		updateField: function( e ) {
+			var $el    = $( e.currentTarget ),
+			    $set   = $el.find( 'a._select' ),
+			    $unset = $el.find( 'a._remove' ),
+			    type   = $el.find( 'input._mi-type' ).val(),
+			    icon   = $el.find( 'input._mi-icon' ).val(),
+			    url    = $el.find( 'input._mi-url' ).val(),
+			    template;
+
+			if ( type === '' || icon === '' || _.indexOf( menuIcons.activeTypes, type ) < 0 ) {
+				$set.text( menuIcons.text.select ).attr( 'title', '' );
+				$unset.addClass( 'hidden' );
+
+				return;
+			}
+
+			if ( miPicker.templates[ type ]) {
+				template = miPicker.templates[ type ];
+			} else {
+				template = miPicker.templates[ type ] = wp.template( 'menu-icons-item-field-preview-' + iconPicker.types[ type ].templateId );
+			}
+
+			$unset.removeClass( 'hidden' );
+			$set.attr( 'title', menuIcons.text.change );
+			$set.html( template({
+				type: type,
+				icon: icon,
+				url:  url
+			}) );
+		},
+
+		/**
+		 * Initialize picker functionality
+		 *
+		 * #fires mi:update
+		 */
+		init: function() {
+			miPicker.createFrame();
+			$( document )
+				.on( 'click', miPicker.wrapClass, miPicker.setUnset )
+				.on( 'mi:update', miPicker.wrapClass, miPicker.updateField );
+
+			// Trigger 'mi:update' event to generate the icons on the item fields.
+			$( miPicker.wrapClass ).trigger( 'mi:update' );
 		}
+	};
 
-		if ( miPicker.templates[ type ] ) {
-			template = miPicker.templates[ type ];
-		} else {
-			template = miPicker.templates[ type ] = wp.template( 'menu-icons-item-field-preview-' + iconPicker.types[ type ].templateId );
-		}
-
-		$unset.removeClass( 'hidden' );
-		$set.attr( 'title', menuIcons.text.change );
-		$set.html( template({
-			type: type,
-			icon: icon,
-			url:  url
-		}) );
-	},
-
-	/**
-	 * Initialize picker functionality
-	 *
-	 * @fires mi:update
-	 */
-	init: function() {
-		miPicker.createFrame();
-		$( document )
-			.on( 'click', miPicker.wrapClass, miPicker.setUnset )
-			.on( 'mi:update', miPicker.wrapClass, miPicker.updateField );
-
-		// Trigger 'mi:update' event to generate the icons on the item fields.
-		$( miPicker.wrapClass ).trigger( 'mi:update' );
-	}
-};
-
-miPicker.init();
+	miPicker.init();
 }( jQuery ) );
