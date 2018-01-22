e112f877df9dcd4787faeda1e3da658069056d81
Rohit Motwani <rohittmotwani@gmail.com>
2018-01-23 03:31:51 +0530

    Minor Fix

--- includes/settings.php
+++ includes/settings.php
@@ -741,24 +741,23 @@ final class Menu_Icons_Settings {
 		}
 		$was_submited = get_option( 'menu_icons_subscribe', false );
 		if ( $was_submited == false ) {
-			$email_output = '<div class="menu-icons-subscribe postbox new-card">';
-			$email_output .= '<h3 class="title">' . esc_html__( 'Get Our Free Email Course', 'menu-icons' ) . '</h3>';
-			$email_output .= '<p id="formdata">' . esc_html__( 'Ready to learn how to reduce your website loading times by half? Come and join the 1st lesson here!', 'menu-icons' ) . ' </p><form class="menu-icons-submit-mail" method="post" onsubmit="return false"><input name="menu_icons_mail" type="email" value="' . get_option( 'admin_email' ) . '" /><input id="ebutton" class="button" type="submit" value="Submit"></form>';
-			$email_output .= '<p id="success">' . esc_html__( 'Thank you for subscribing! You have been added to the mailing list and will receive the next email information in the coming weeks. If you ever wish to unsubscribe, simply use the "Unsubscribe" link included in each newsletter.', 'menu-icons' ) . '</p>';
-			$email_output .= '</div>';
+			$box_data .= '<div class="menu-icons-subscribe postbox new-card">';
+			$box_data .= '<h3 class="title">' . esc_html__( 'Get Our Free Email Course', 'menu-icons' ) . '</h3>';
+			$box_data .= '<p id="formdata">' . esc_html__( 'Ready to learn how to reduce your website loading times by half? Come and join the 1st lesson here!', 'menu-icons' ) . ' </p><form class="menu-icons-submit-mail" method="post" onsubmit="return false"><input name="menu_icons_mail" type="email" value="' . get_option( 'admin_email' ) . '" /><input id="ebutton" class="button" type="submit" value="Submit"></form>';
+			$box_data .= '<p id="success">' . esc_html__( 'Thank you for subscribing! You have been added to the mailing list and will receive the next email information in the coming weeks. If you ever wish to unsubscribe, simply use the "Unsubscribe" link included in each newsletter.', 'menu-icons' ) . '</p>';
+			$box_data .= '</div>';
+			$box_data .= '</div>';
+			$box_data .= '<script>';
+			$box_data .= '$(function(){';
+			$box_data .= '$("#success").hide();';
+			$box_data .= '$("#ebutton").on("click", function(){';
+			$box_data .= '$("#formdata").hide();';
+			$box_data .= '$(".menu-icons-submit-mail").hide();';
+			$box_data .= '$("#success").show();';
+			$box_data .= '});';
+			$box_data .= '});';
+			$box_data .= '</script>';
 		}
-		$box_data .= $email_output;
-		$box_data .= '</div>';
-		$box_data .= '<script>';
-		$box_data .= '$(function(){';
-		$box_data .= '$("#success").hide();';
-		$box_data .= '$("#ebutton").on("click", function(){';
-		$box_data .= '$("#formdata").hide();';
-		$box_data .= '$(".menu-icons-submit-mail").hide();';
-		$box_data .= '$("#success").show();';
-		$box_data .= '});';
-		$box_data .= '});';
-		$box_data .= '</script>';
 
 		$js_data = apply_filters(
 			'menu_icons_settings_js_data',
