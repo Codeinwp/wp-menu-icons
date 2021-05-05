b263673d55ee0168eaa281c37c5e8738d86fb902
selul <marius.cristea@vertistudio.com>
2021-05-05 21:55:19 +0300

    moving to github actions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,39 @@
-/**
- * Grunt File
- *
- * @package feedzy-rss-feeds
- */
 /* jshint node:true */
 /* global require */
+
 module.exports = function (grunt) {
 	'use strict';
+	grunt.initConfig({
+		wp_readme_to_markdown: {
+			files: {
+				'readme.md': 'readme.txt'
+			},
+		},
+		version: {
 
-	var loader = require('load-project-config'),
-		config = require('grunt-plugin-fleet');
-	config = config();
-	loader(grunt, config).init();
+			project: {
+				src: [
+					'package.json'
+				]
+			},
+			style: {
+				options: {
+					prefix: 'Version\\:\.*\\s'
+				},
+				src: [
+					'menu-icons.php'
+				]
+			},
+			class: {
+				options: {
+					prefix: '\\.*VERSION\.*\\s=\.*\\s\''
+				},
+				src: [
+					'menu-icons.php',
+				]
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-version');
+	grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
 };
